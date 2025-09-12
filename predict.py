c1c148bfed2f96fb0260275db27259680987d7b2
rakeshvar <rakeshvar@gmail.com>
2025-09-12 11:17:23 -0700

    Working predict.py from a saved checkpoint model.

--- predict.py
+++ predict.py
@@ -1,22 +1,23 @@
+import math
+
 import argparse
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
 from tensorflow import keras
-import matplotlib.pyplot as plt
 import editdistance
 
+from deformer.deformer import Deformer
 from model_builder import CRNNReshape, CTCLayer
 from post_process import PostProcessor
-# from post_process_eval import show_prediction
 
 try:
     import telugu as lang
-    from Lekhaka import Scribe, Deformer, Noiser, DataGenerator
+    from Lekhaka import Scribe, DataGenerator
 except ModuleNotFoundError:
     import Lekhaka.telugu as lang
-    from Lekhaka.Lekhaka import Scribe, Deformer, Noiser, DataGenerator
+    from Lekhaka.Lekhaka import Scribe, DataGenerator
 
 # -----------------------------
 # Helper: build datagen
@@ -37,7 +38,8 @@ ctc_model = keras.models.load_model(
     checkpoint,
     compile=False,
     custom_objects={"CRNNReshape": CRNNReshape,
-                    "CTCLayer": CTCLayer}
+                    "CTCLayer": CTCLayer,
+                    "Deformer": Deformer}
 )
 prediction_model = keras.models.Model(
     ctc_model.get_layer(name="image").output,
@@ -45,33 +47,27 @@ prediction_model = keras.models.Model(
 )
 
 img_shape = prediction_model.get_layer("image").output.shape
-print("Image shape: ", img_shape)#, out_shape)
+print("Image shape: ", img_shape)
 _, height, wd_in, ch_in = img_shape
 
 out_shape = prediction_model.get_layer("softmax").output.shape
 _, wd_out, num_classes = out_shape
 print("Out shape: ", out_shape)
 
-assert wd_in % wd_out == 0, f"Width In {wd_in} not divisible by Width Out {wd_out}"
-wd_scaled_down_by = wd_in // wd_out
-
 # -----------------------------
 # Datagen, Printer
 # -----------------------------
 scribe_args = {"height": height, "hbuffer": 5, "vbuffer": 0, "nchars_per_sample": args.chars_per_sample}
-noise_args = {"num_blots": 0, "erase_fraction": 0., "minsize": 0, "maxsize": 0}
-elastic_args = {"translation": 0, "zoom": 0, "elastic_magnitude": 0, "sigma": 1, "angle": 0, "nearest": True}
-
+dummy = lambda x: x
 scriber = Scribe(lang, **scribe_args)
-deformer = Deformer(**elastic_args)
-noiser = Noiser(**noise_args)
-datagen = DataGenerator(scriber, deformer, noiser, batch_size)
+datagen = DataGenerator(scriber, dummy, dummy, batch_size)
 printer = PostProcessor(lang.symbols)
 
 def one_batch():
     images, labels, img_lens, lbl_lens = datagen.get()
     probs = prediction_model.predict(images)
-    prob_lens = img_lens // wd_scaled_down_by
+    wd_scaled_down_by = images.shape[-2] // probs.shape[-2]
+    prob_lens = [math.ceil(img_len / wd_scaled_down_by) for img_len in img_lens]
     beams = printer.beam_decode(probs, prob_lens)
 
     print(lbl_lens)
@@ -79,10 +75,11 @@ def one_batch():
     i = 0
     for label, image, img_len, lbl_len, prob, prob_len in zip(labels, images, img_lens, lbl_lens, probs, prob_lens):
         label = label[:lbl_len]
-        image = image.squeeze()[::2, :img_len:2]
-        prob = prob[:prob_len, :]
+        image = image.squeeze()
+        image2 = image[::2, :img_len:2]
+        prob2 = prob[:prob_len, :]
         shown_chars = printer.labels_to_chars(label)
-        seen_labels = printer.decode(prob)
+        seen_labels = printer.decode(prob2)
         greedy_decoded = printer.labels_to_chars(seen_labels)
         nmissed = len(set(label)-set(seen_labels))
         beam_decoded = beams[i]
@@ -91,12 +88,12 @@ def one_batch():
         print(i)
         if eddist > -1:
             print("EDIT DISI: ", eddist)
-            printer.show_all(label, image, prob, True)
-            # show_prediction(image, dilate(prob, wd_scaled_down_by, 1), greedy_decoded, beam_decoded)
-            # img = np.vstack((as255(1-image),
-            #                  dilate(as255((prob.T)), wd_scaled_down_by, wd_scaled_down_by)))
-            # img = Image.fromarray(img)
-            # img.show()
+            printer.show_all(label, image2, prob2, True)
+            img255 = as255(1 - image)
+            prob255 = dilate(as255(prob.T), wd_scaled_down_by, wd_scaled_down_by)
+            img = np.vstack((img255, prob255))
+            img = Image.fromarray(img)
+            img.show()
 
         i += 1
 
@@ -106,10 +103,15 @@ def as255(v):
 
 
 def dilate(a, dh, dw):
-    h, w = a.shape#    H, W
-    a1 = np.stack((a,)*dh, axis=1)     # H, Dh, W
-    a2 = np.stack((a1,)*dw, axis=-1) # H, Dh, W, Dw
-    return a2.reshape((h*dh, w*dw))
+    h, w = a.shape                     # H ⨉ W
+    a1 = np.stack((a,)*dh, axis=1)     # H ⨉ Dh ⨉ W
+    a2 = np.stack((a1,)*dw, axis=-1)   # H ⨉ Dh ⨉ W ⨉ Dw
+    return a2.reshape((h*dh, w*dw))    # HDh ⨉ WDw
 
 
-one_batch()
+while True:
+    one_batch()
+    try:
+        input("Want to see one more? Press Enter. Else Ctrl-Z.")
+    except KeyboardInterrupt:
+        break
